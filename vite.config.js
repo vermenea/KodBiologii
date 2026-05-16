08d7d908805ac1be5e21ae26307c609ea6541961
Natalia Zagórska <vermenea@gmail.com>
2026-05-16 19:46:41 +0200

    fix: vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,13 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          'framer-motion': ['framer-motion'],
-          'react-icons': ['react-icons'],
+        manualChunks: (id) => {
+          if (id.includes('framer-motion')) {
+            return 'framer-motion';
+          }
+          if (id.includes('react-icons')) {
+            return 'react-icons';
+          }
         },
       },
     },
